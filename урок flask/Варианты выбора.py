e96225abd18003caaa71e2b519655c5fc328af6c
Potom-python <timofei50200@gmail.com>
2025-03-04 18:07:47 +0500

    Варианты выбора и урок flask-wtf

---
+++ урок flask/Варианты выбора.py
@@ -0,0 +1,39 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/choice/<planet_name>')
+def choise(planet_name):
+    return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet" 
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                            crossorigin="anonymous">
+                            <title>Варианты выбора</title>
+                          </head>
+                          <body>
+                            <h1>Мое предложение: {planet_name}</h1>
+                            <h2>Эта планета близка к земле;</h2>
+                            <h3 class="alert alert-success" role="alert">
+                                На ней много необходимых ресурсов;
+                            </h3>
+                            <h3 class="alert alert-dark" role="alert">
+                                На ней есть вода и атмосфера;
+                            </h3>
+                            <h3 class="alert alert-warning" role="alert">
+                                На ней есть небольшое магнитное поле;
+                            </h3>
+                            <h3 class="alert alert-danger" role="alert">
+                                Наконец, она просто красива!
+                            </h3>
+                          </body>
+                        </html>'''
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
