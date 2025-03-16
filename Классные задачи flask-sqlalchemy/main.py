be72a07d4f35c9e33894e57d4a000570cc13da24
Potom-python <timofei50200@gmail.com>
2025-03-16 23:02:33 +0300

    Добавляем капитана

---
+++ Классные задачи flask-sqlalchemy/main.py
@@ -0,0 +1,55 @@
+from data import db_session
+from data.users import User
+
+
+def main():
+    db_session.global_init("db/jobs.db")
+    db_sess = db_session.create_session()
+
+    user = User()
+    user.surname = "Scott"
+    user.name = "Ridley"
+    user.age = 21
+    user.position = "captain"
+    user.speciality = "research engineer"
+    user.address = "module_1"
+    user.email = "scott_chief@mars.org"
+    db_sess.add(user)
+    db_sess.commit()
+
+    user = User()
+    user.surname = "Karl"
+    user.name = "Velikiy"
+    user.age = 55
+    user.position = "captain"
+    user.speciality = "pilot"
+    user.address = "module_2"
+    user.email = "karl_chief@mars.org"
+    db_sess.add(user)
+    db_sess.commit()
+
+    user = User()
+    user.surname = "Pol"
+    user.name = "Polov"
+    user.age = 25
+    user.position = "navigator"
+    user.speciality = "admin"
+    user.address = "module_3"
+    user.email = "pol_admin@mars.org"
+    db_sess.add(user)
+    db_sess.commit()
+
+    user = User()
+    user.surname = "Kirill"
+    user.name = "Ismagilov"
+    user.age = 16
+    user.position = "janitor"
+    user.speciality = "programmer"
+    user.address = "module_4"
+    user.email = "kirill_janitor@mars.org"
+    db_sess.add(user)
+    db_sess.commit()
+
+
+if __name__ == '__main__':
+    main()
